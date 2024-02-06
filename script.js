630ac78f4610255f5b6b337d3cffa361d2dda37a
MarsLucifer <76066235+MarsLucifer@users.noreply.github.com>
2024-02-07 01:17:35 +0530

    sorted-countries

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const countryDrop = document.querySelector('#country-selector');
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => {
+            // Sort the countries by name in alphabetical order
+            const sortedCountries = data.sort((a, b) =>
+                a.name.common.localeCompare(b.name.common, 'en', { sensitivity: 'base' })
+            );
 
-    const countryDrop = document.querySelector('#country-selector')
-    fetch('https://restcountries.com/v3.1/all').then(res => {
-        return res.json();
-    }).then(data => {
-        let output ="";
-        data.forEach(country => {
-            output += `<option>${country.name.common}</option>`;
+            let output = sortedCountries.map(country =>
+                `<option value="${country.cca2}">${country.name.common}</option>`
+            ).join(''); // Join the array of strings into a single string
+
+            countryDrop.innerHTML = output;
         })
-        countryDrop.innerHTML = output
-    }).catch(err => {
-        console.log(err);
-    })
-})
+        .catch(err => {
+            console.log(err);
+        });
+});
